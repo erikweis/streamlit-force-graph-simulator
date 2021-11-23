d386a890e7e37b71f05aebdedc52e7fcf385d9b5
erikweis <32458904+erikweis@users.noreply.github.com>
2021-11-23 17:48:27 -0500

    update gitignore

---
+++ streamlit_force_graph_simulator/frontend/build/precache-manifest.02214d66d363e2be5786f2d7999169d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636f8fbf567f5e45d6ec109476c53622",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd0f9a915d239a5446c3",
+    "url": "./static/js/2.ddfd28cd.chunk.js"
+  },
+  {
+    "revision": "164933573ecce9c5fa8212c1a75cc591",
+    "url": "./static/js/2.ddfd28cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816ea0e446d7a0ce0e20",
+    "url": "./static/js/main.435fb193.chunk.js"
+  },
+  {
+    "revision": "c6c613ae59ef7502cd97",
+    "url": "./static/js/runtime-main.3ca778a4.js"
+  },
+  {
+    "revision": "9cfe50b68736f61d061af526da6aaf14",
+    "url": "./static/media/bezier.9cfe50b6.cjs"
+  }
+]);
